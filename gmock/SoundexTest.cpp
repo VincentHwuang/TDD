e014fa0f004781a83e3c33ab78f6d7b29bbd14e8
VincentHwuang <Vincent_Hwuang@hotmail.com>
2017-10-12 20:04:38 +0800

    go further...

--- gmock/SoundexTest.cpp
+++ gmock/SoundexTest.cpp
@@ -1,15 +1,48 @@
 #include"gmock/gmock.h"
+#include"Soundex.h"
 
-class CSoundex
+using namespace testing;
+
+//Using 'fixture' class to declare the same class instance using several times in different tests
+class CSoundexEncoding: public Test
 {
-
+	public:
+		CSoundex soundex;
 };
 
-TEST(SoundexEncoding,RetainSoleLetterOfOneLetterWorld)
+  //Tests without using 'fixture' class
+//TEST(SoundexEncoding,RetainSoleLetterOfOneLetterWorld)
+//{
+//	CSoundex soundex;
+//	auto encoded=soundex.encode("A");
+//
+//	ASSERT_THAT(encoded,Eq("A000"));
+//}
+//
+//TEST(SoundexEncoding,PadsWithZerosToEnsureThreeDigits)
+//{
+//	CSoundex soundex;
+//	auto encoded=soundex.encode("l");
+//	ASSERT_THAT(encoded,Eq("l000"));
+//}
+
+//To use 'fixture' class,we must use 'TEST_F'('F' for 'fixture') instead of 'TEST' macro
+TEST_F(CSoundexEncoding,PadsWithZerosToEnsureThreeDigits)
 {
-	CSoundex soundex;
+//	auto encoded=soundex.encode("l");
+//	ASSERT_THAT(encoded,Eq("l000"));
+	ASSERT_THAT(soundex.encode("l"),Eq("l000"));
 }
 
+TEST_F(CSoundexEncoding,RetainsSoleLetterOfOneLetterWord)
+{
+	ASSERT_THAT(soundex.encode("Ab"),Eq("A100"));
+}
+
+TEST_F(CSoundexEncoding,ReplacesConsonantsWithAppropriateDigits)
+{
+	ASSERT_THAT(soundex.encode("Ax"),Eq("A200"));
+}
 int main(int argc,char** argv)
 {
 	testing::InitGoogleMock(&argc,argv);
